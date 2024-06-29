ff3b4b1281af9315d01669ff70e884f73277e501
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-06-29 13:33:45 -0500

    make font bold

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;    /* -c option; centers dmenu on screen */
 static int min_width = 500; /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"Jetbrains Mono NerdFont:size=12"};
+static const char *fonts[] = {"Jetbrains Mono NerdFont:size=12:style=Bold"};
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
